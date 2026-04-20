261ac9c784952685b2c9e866d53da2c8d1192da3
vaibhav chaturvedi <vaibhavchaturvedi.work@gmail.com>
2026-04-21 00:15:46 +0530

    adding start screen widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,15 @@ import 'package:flutter/material.dart';
 import 'start_screen.dart';
 
 void main() {
-  runApp(MaterialApp(home: Scaffold(body: StartScreen(),),),);
+  runApp(
+    MaterialApp(
+      home: Scaffold(body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.deepPurple, Color.fromARGB(255, 107, 15, 168)], 
+            begin: Alignment.topLeft, end: Alignment.bottomRight)
+        ),
+        child: StartScreen(),),),
+    ),
+  );
 }
